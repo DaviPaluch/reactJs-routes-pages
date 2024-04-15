af41ecc10ff8447fc3dbf9c6f882113e88c9c631
Davi Paluch <101030497+DaviPaluch@users.noreply.github.com>
2024-04-15 03:10:14 +0000

    aprendendo a fazer um Loading Data com React-Router

--- React-Router-Tutorial/src/main.jsx
+++ React-Router-Tutorial/src/main.jsx
@@ -2,18 +2,20 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
-  RouterProvider,
+  RouterProvider
 } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
